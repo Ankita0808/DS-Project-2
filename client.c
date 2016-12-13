05feb8770e698ee539a7f99853447054c79b0783
Donald Kline, Jr <don.jr.mp21@gmail.com>
2016-12-12 20:26:38 -0500

    Added good, safe parts of client.c dealing with redundant servers, and added an extra friendliness to the user, where if a server is down at the start, it no longer has to prompt the user twice. This client.c file works with all existing versions of all other files (with the exception of lib.h, which it needs to have SERVER_FILENAME2 defined)

--- client.c
+++ client.c
@@ -1,17 +1,25 @@
 #include "network_function.h"
 #include <math.h>
 
-int readServerIP (char *server_ip, char *server_port)
+int readServerIP (char *server_ip, char *server_port, int redundancy)
 {
 	// read IP address and port from file define in header.h
 	FILE *ptr_file;
 	char buf[100];
 	// read the global file
-	ptr_file =fopen(SERVER_FILENAME, "r");
+	if (redundancy==0)
+		ptr_file =fopen(SERVER_FILENAME, "r");
+	else if (redundancy==1)
+		ptr_file =fopen(SERVER_FILENAME2, "r");
+	else
+	{
+		printf("This server currently only has a redundancy of 2 (coding error)");
+		return 0;
+	}
 	if (!ptr_file)
 	{
+		//printf("no file open");
 		return 0;
-		printf("no file open");
 	}
 	if(fgets(buf,1000, ptr_file)!=NULL)
 	{
@@ -35,6 +43,9 @@ int main()
 	char URL[MAX_URL_LEN], buf[MAX_BUF_LEN], server_ip[100], server_port[100];
 	long int segment_size;
 	int choice=1;
+	const int max_redundancy= 2;
+	int num_redundancy_tries = 0;
+	int current_redundant_server=0;
 	int16_t reply;
 	char query_string[MAX_QUERY_LEN], **doc;
 	
@@ -44,28 +55,59 @@ int main()
 	timestamp_t begin, end, average[times], standard, real_average;
 	
 	
+	int fullfilled_choice = 1;
 	while (choice == 1 || choice == 2)
 	{
-		printf("Press:\n1 to index\n2 to query\n0 to exit\n");
-		scanf("%d", &choice);
+
+		if (num_redundancy_tries==max_redundancy)
+		{
+			perror("ERROR: All name servers are offline!");
+			perror("Exiting...");
+			exit(1);
+		}
+
+		if (fullfilled_choice == 1)
+		{
+			printf("Press:\n1 to index\n2 to query\n0 to exit\n");
+			scanf("%d", &choice);
+		}
 		if (choice == 1)
 		{
-			printf("Input Index Directory:");
-			while ((c = getchar()) != '\n' && c != EOF);
-			scanf("%s", URL);
-			printf("Input Segment Size (KB):");
-			scanf("%ld", &segment_size);
-			while ((c = getchar()) != '\n' && c != EOF);
+			if (fullfilled_choice == 1) //If the client is not waiting on a previous request
+			{
+				printf("Input Index Directory:");
+				while ((c = getchar()) != '\n' && c != EOF);
+				scanf("%s", URL);
+				printf("Input Segment Size (KB):");
+				scanf("%ld", &segment_size);
+				while ((c = getchar()) != '\n' && c != EOF);
+			}	
 			
-			readServerIP(server_ip, server_port);
+			//Read the name server IP from the shared filespace. 
+			if (readServerIP(server_ip, server_port, current_redundant_server)==0)
+			{
+				num_redundancy_tries++;
+				printf("DEBUG: name server number %d was not initialized (can't read its file)",current_redundant_server);
+				fullfilled_choice = 0; //Do not ask user again
+				current_redundant_server=(current_redundant_server+1)%max_redundancy;
+				continue;
+			}
 			
 			// open socket to connect to server
 			if ((sockfd = connectTCP_server(server_ip, server_port)) == 0)
 			{
-				perror("Connection to server:");
+				//perror("Connection to server:");
+				printf("DEBUG: name server number %d is offline, switching to next redundant server",current_redundant_server);
+				current_redundant_server=(current_redundant_server+1)%max_redundancy;
+				fullfilled_choice=0;
 				continue;
 			}
 			
+			//Connected fine to one of the servers, so give the other one a chance to come back online if it was down
+			//In other words, no longer assume it is down if it was previously
+			num_redundancy_tries=0;
+			fullfilled_choice=1;
+
 			memset(buf, 0, sizeof(buf));
 		
 			// pack the register data
@@ -81,13 +123,13 @@ int main()
 			
 			if ((numbytes = recv(sockfd, buf, MAX_BUF_LEN, 0)) == -1)
 			{
-				perror("recevie from server");
+				perror("receive from server");
 				continue;
 			}
 			
 			unpack(buf,"h", &reply);
 			if (reply == 0)
-				printf("Index not succeeded: wrong directory path\n");
+				printf("Index did not succeed: wrong directory path\n");
 			else 
 				printf("Index succeeded\n");
 			// receive result
@@ -104,26 +146,44 @@ int main()
 			// begin = get_timestamp();
 			// count++;
 			
-			printf("Input Query:\n");
-			query_string[0] = '\0'; /* Ensure empty line if no input delivered */
-			query_string[sizeof(query_string)-1] = ~'\0';  /* Ensure no false-null at end of buffer */
+			if (fullfilled_choice==1) //If the client is not waiting on a previous request
+			{
+				printf("Input Query:\n");
+				query_string[0] = '\0'; /* Ensure empty line if no input delivered */
+				query_string[sizeof(query_string)-1] = ~'\0';  /* Ensure no false-null at end of buffer */
+				
+				//flush the stdin
+				
+				while ((c = getchar()) != '\n' && c != EOF);
+				
+				fgets(query_string, sizeof(query_string), stdin);
+				
+				printf("\nQuery is: %s\n", query_string);
+			}
 			
-			//flush the stdin
-			
-			while ((c = getchar()) != '\n' && c != EOF);
-			
-			fgets(query_string, sizeof(query_string), stdin);
-			
-			printf("\nQuery is: %s\n", query_string);
-			
-			readServerIP(server_ip, server_port);
+			if (readServerIP(server_ip, server_port,current_redundant_server)==0)
+			{
+				num_redundancy_tries++;
+				printf("DEBUG: name server number %d was not initialized (can't read its file)",current_redundant_server);
+				current_redundant_server=(current_redundant_server+1)%max_redundancy;
+				fullfilled_choice=0;
+				continue;
+			}
 			
 			// open socket to connect to namenode
 			if ((sockfd = connectTCP_server(server_ip, server_port)) == 0)
 			{
-				perror("Connection to namenode:");
+				//perror("Connection to namenode:");				
+				printf("DEBUG: name server number %d is offline, switching to next redundant server",current_redundant_server);
+				current_redundant_server=(current_redundant_server+1)%max_redundancy;
+				fullfilled_choice=0;
 				continue;
 			}
+
+			//Connected fine to one of the servers, so give the other one a chance to come back online if it was down
+			//In other words, no longer assume it is down if it was previously
+			num_redundancy_tries=0;
+			fullfilled_choice=1;
 			
 			memset(buf, 0, sizeof(buf));
 		
